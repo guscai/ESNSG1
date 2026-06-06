20ff0b708c60f32b3a38076d1fc5f1c1a75d4525
guscai <86561048+guscai@users.noreply.github.com>
2026-06-06 16:33:59 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,13 +1,10 @@
-# database.py
-from supabase import create_client
 import streamlit as st
+from supabase import create_client
 
+# Configuración usando Streamlit Secrets (lo que configuraste en la nube)
 url = st.secrets["SUPABASE_URL"]
 key = st.secrets["SUPABASE_KEY"]
 supabase = create_client(url, key)
 
 def insertar_persona(data):
     return supabase.table("personas").insert(data).execute()
-
-def obtener_personas():
-    return supabase.table("personas").select("*").execute().data
